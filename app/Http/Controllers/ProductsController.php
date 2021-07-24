d25edca4c343c44f24d05e51c9f0a914d2e669e9
Ádám Kurucz <adam.kurucz@attrecto.com>
2021-07-24 20:26:50 +0200

    fix

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,6 +14,12 @@ class ProductsController extends Controller
         $pageSize = $request->get('pageSize', 15);
         $sortBy = $request->get('sortBy','name');
         $orderBy = $request->get('orderBy','asc');
+        $priceFrom = $request->get('priceFrom');
+        $widthFrom = $request->get('widthFrom');
+        $widthTo = $request->get('widthTo');
+        $thicknessFrom = $request->get('thicknessFrom');
+        $thicknessTo = $request->get('thicknessTo');
+        $sale = $request->get('sale');
 
         $query = Products::query();
         if(!empty($searchText)) {
@@ -22,6 +28,18 @@ class ProductsController extends Controller
                 ->orWhere('color','like', '%'.$searchText . '%');
                 
             });
+        }
+        if((!empty($priceFrom)) && (!empty($priceTo))) {
+            $query->whereBetween('gross_price', [$priceFrom,$priceTo]);
+        }
+        if((!empty($widthFrom)) && (!empty($widthTo))) {
+            $query->whereBetween('width', [$widthFrom,$widthTo]);
+        }
+        if((!empty($thicknessFrom)) && (!empty($thicknessTo))) {
+            $query->whereBetween('thickness', [$thicknessFrom,$thicknessTo]);
+        }
+        if(!empty($sale)) {
+            $query->where('sale', '<>', 'NULL');
         }
         $projects = $query->orderBy($sortBy,$orderBy)->paginate($pageSize);
         
