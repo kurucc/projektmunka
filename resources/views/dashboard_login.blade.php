1d8a0dfa9824dc8f52b25731cb60e74138539256
Fruzsina Mihalovics <fruzsina.mihalovics@attrecto.com>
2021-08-02 21:14:48 +0200

    dashboard

--- resources/views/dashboard_login.blade.php
+++ resources/views/dashboard_login.blade.php
@@ -1,3 +1,183 @@
 @include('header')
 <a href={{ url('logout') }}>Logout</a>
+<h2>Helló USERNAME!</h2>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h4>Profil adatok</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+                          <label>Keresztnév</label> <br>
+                          <input
+                            type="text"
+                            class="form-control-sm"
+                            name="firstname"
+                            disabled
+                            placeholder="FIRSTNAME"
+                          />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Vezetéknév</label> <br>
+                          <input
+                            type="text"
+                            name="lastname"
+                            class="form-control-sm"
+                            disabled
+                            placeholder="LASTNAME"
+                          />
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <label>Felhasználónév</label> <br>
+                          <input
+                            type="text"
+                            name="felhasználónév"
+                            class="form-control-sm"
+                            disabled
+                            placeholder="USERNAME"
+                          />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Születési idő</label> <br>
+                          <input
+                            type="date"
+                            name="születésnap"
+                            class="form-control-sm"
+                            disabled
+                          />
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <label>Telefonszám</label> <br>
+                          <input
+                            type="text"
+                            name="telefonszám"
+                            class="form-control-sm"
+                            placeholder="PHONENUMBER"
+                            disabled
+                          />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Email cím</label> <br>
+                          <input
+                            type="email"
+                            class="form-control-sm"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                            name="email"
+                            disabled
+                            placeholder="EMAIL"
+                          />
+                        </div>
+                      </div>
+                      <div class="row">
+                      <div class="col-sm-6">
+                          <label>Új jelszó beállítása</label> <br>
+                          <input
+                            type="password"
+                            class="form-control-sm"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                            name="newPassword"
+                            disabled
+                            placeholder="password"
+                          /> <br>
+                          <label>Új jelszó újra</label> <br>
+                          <input
+                            type="password"
+                            class="form-control-sm"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                            name="newPasswordAgain"
+                            disabled
+                            placeholder="password"
+                          />
+                        </div>
+                      </div>
+
+                      <div class="row">
+                      <label><h5>Szállítási adatok</h5></label>
+                     <div class="col-sm-6">
+                          <label>Irányítószám</label> <br>
+                          <input type="text" class="form-control-sm" name="delivery_zip" disabled placeholder="ZIP" />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Település</label> <br>
+                          <input type="text" class="form-control-sm" name="delivery_city" disabled placeholder="CITY"/>
+                        </div>
+                      </div>
+                      <div class="row ms-0">
+                          <label>Cím</label> <br>
+                          <input type="text" class="form-control-sm" name="delivery_zip" disabled placeholder="ADDRESS"/>
+                      </div>
+
+
+                        <!--If a person this one shows up-->
+                        <div class="row maganszemely">
+                        <label><h5>Számlázási adatok</h5></label>
+                         <div class="col-sm-6">
+                          <label>Keresztnév</label> <br>
+                          <input
+                            type="text"
+                            class="form-control-sm"
+                            name="firstname"
+                            disabled
+                            placeholder="FIRSTNAME"
+                          />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Vezetéknév</label> <br>
+                          <input
+                            type="text"
+                            name="lastname"
+                            class="form-control-sm"
+                            disabled
+                            placeholder="LASTNAME"
+                          />
+                        </div>
+
+                      </div>
+                        <!--If a company this one shows up-->
+                        <div class="row company">
+                        <label><h5>Számlázási adatok</h5></label>
+                         <div class="col-sm-6">
+                          <label>Cég név</label> <br>
+                          <input
+                            type="text"
+                            class="form-control-sm"
+                            name="firstname"
+                            disabled
+                            placeholder="COMPANYNAME"
+                          />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Adószám</label> <br>
+                          <input
+                            type="text"
+                            name="lastname"
+                            class="form-control-sm"
+                            disabled
+                            placeholder="TAXNUMBER"
+                          />
+                        </div>
+                      </div>
+
+                    <div class="row">
+                     <div class="col-sm-6">
+                          <label>Irányítószám</label> <br>
+                          <input type="text" class="form-control-sm" name="delivery_zip" disabled placeholder="ZIP" />
+                        </div>
+                        <div class="col-sm-6">
+                          <label>Település</label> <br>
+                          <input type="text" class="form-control-sm" name="delivery_city" disabled placeholder="CITY"/>
+                        </div>
+                      </div>
+                      <div class="row ms-0 pb-5">
+                          <label>Cím</label> <br>
+                          <input type="text" class="form-control-sm" name="delivery_zip" disabled placeholder="ADDRESS"/>
+                      </div>
+</div>
+</div>
 @include('footer')
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -11,6 +11,8 @@
         <link rel="stylesheet" href={{ URL::asset('css/sidebar.css') }} />
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+       <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"  media="screen">
+       <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -31,6 +33,7 @@
             crossorigin="anonymous"
         ></script>
         <script src={{ URL::asset('js/sidebar.js') }}></script>
+         <script src={{ URL::asset('js/selector.js') }}></script>
         <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
@@ -41,6 +44,7 @@
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css"
         />
+
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-custom">
@@ -169,17 +173,20 @@
                             Kalkulátor
                         </h6>
                     </div>
+                    <div class="item-open">
+                        <div class="selector">
+                            <div class="price-slider">
+                                <div id="slider-range" class="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
+                                    <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
+                                    <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
+                                </div>
+                                <span id="min-price" data-currency="€" class="slider-price">0</span> <span class="seperator">-</span> <span id="max-price" data-currency="€" data-max="3500"  class="slider-price">3500 +</span>
+                            </div>
+                        </div>
+                          </div>
                 </div>
             </nav>
         </div>
-        @foreach ($projects as $key => $value)
-                <p>{{ $value->id }}</p>
-                <p>{{ $value->name }}</p>
-                <p>{{ $value->sale }}</p>
-                <p>{{ $value->gross_price }}</p>
-                <br>
-            @endforeach
 
-            {{ $projects->appends($_GET)->links()}}
     </body>
 </html>
