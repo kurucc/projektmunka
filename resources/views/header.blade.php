de75dd60c1fbb038c98a15af795120d087a9739c
Fruzsina Mihalovics <fruzsina.mihalovics@attrecto.com>
2021-08-29 18:03:35 +0200

    products page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -14,6 +14,11 @@
       rel="stylesheet"
       href="{{ URL::asset('css/dashboard.css') }}"
     />
+    <link
+      rel="stylesheet"
+      href="{{ URL::asset('css/products.css') }}"
+    />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src={{ URL::asset('js/login.js') }}></script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -71,10 +71,7 @@
         </div>
       </div>
     </nav>
-    <div class="flex-container">
-      <div class="flex-box" style="background-color: black">dasda</div>
-      <div class="flex-box" style="background-color: burlywood">dadas</div>
-    </div>
+
     <div class="bg"></div>
   </body>
 </html>
